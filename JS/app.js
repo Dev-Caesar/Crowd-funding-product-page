356f0c12d3d3525f273f912c2dd61ab1cf7785cf
Nweke Caesar <caesarridd@gmail.com>
2022-08-14 13:41:42 +0100

    fixed mobile menu bug

--- JS/app.js
+++ JS/app.js
@@ -8,33 +8,30 @@ const closeIcon = document.querySelector('.close-icon');
 const overlay = document.querySelector('.overlay');
 let hidden = true;
 
+const openMobileMenu = function (e) {
+  navMenu.classList.remove('hidden-menu');
+  hamburgerMenu.classList.add('hidden');
+  closeIcon.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  hidden = false;
+};
+
+const closeMobileMenu = function (e) {
+  navMenu.classList.add('hidden-menu');
+  hamburgerMenu.classList.remove('hidden');
+  closeIcon.classList.add('hidden');
+  overlay.classList.add('hidden');
+  hidden = true;
+};
+
 menuBtn.addEventListener('click', function (e) {
   if (hidden) {
-    navMenu.classList.remove('hidden-menu');
-    hamburgerMenu.classList.add('hidden');
-    closeIcon.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-    hidden = false;
+    openMobileMenu();
   } else {
-    navMenu.classList.add('hidden-menu');
-    hamburgerMenu.classList.remove('hidden');
-    closeIcon.classList.add('hidden');
-    overlay.classList.add('hidden');
-    hidden = true;
+    closeMobileMenu();
   }
 });
 
 overlay.addEventListener('click', function (e) {
-  navMenu.classList.add('hidden-menu');
-  hamburgerMenu.classList.remove('hidden');
-  closeIcon.classList.add('hidden');
-  overlay.classList.add('hidden');
-  hidden = true;
-});
-navMenu.addEventListener('click', function (e) {
-  navMenu.classList.add('hidden-menu');
-  hamburgerMenu.classList.remove('hidden');
-  closeIcon.classList.add('hidden');
-  overlay.classList.add('hidden');
-  hidden = true;
+  closeMobileMenu();
 });
